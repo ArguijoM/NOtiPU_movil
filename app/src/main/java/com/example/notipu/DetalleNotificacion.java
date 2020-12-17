99d047c25f02cc225e8bdfedd6f09369d42d7907
argelos <arguijomanuel@gmail.com>
2020-12-17 12:15:29 -0600

    Primer entrega

--- app/src/main/java/com/example/notipu/DetalleNotificacion.java
+++ app/src/main/java/com/example/notipu/DetalleNotificacion.java
@@ -1,5 +1,6 @@
 package com.example.notipu;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,11 +10,25 @@ import android.widget.TextView;
 public class DetalleNotificacion extends AppCompatActivity {
     int id;
     TextView titulo,mensaje;
+    private ActionBar actionBar;
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_notificacion);
+        //Inicializacion
+        actionBar = getSupportActionBar();
+        //Titulo
+        actionBar.setTitle("Notificación");
+        //Boton Negro
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
 
         Bundle b = getIntent().getExtras();
         id = b.getInt("id");
@@ -22,5 +37,7 @@ public class DetalleNotificacion extends AppCompatActivity {
         mensaje = findViewById(R.id.mensaje);
         titulo.setText("Becas 2010");
         mensaje.setText("Les recordamos que deben subir sus documentos antes de la fecha mencionada para que puedan quedar inscritos");
+
+
     }
 }
